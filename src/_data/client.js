151255dee38db12baef81524f2b5202fbc805934
troy <troywebb95@gmail.com>
2024-11-10 14:57:36 +0000

    update information

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,21 @@
 module.exports = {
-    name: "Pixel Perfect Solutions",
-    email: "hello@pixelperfect.dev",
-    phoneForTel: "555-123-4567",
-    phoneFormatted: "(555) 123-4567",
+    name: "Webber Creative",
+    email: "troy@webbercreative.com",
+    phoneForTel: "720-123-4567",
+    phoneFormatted: "(720) 123-4567",
     address: {
-        lineOne: "123 Digital Avenue",
-        lineTwo: "Suite 404",
-        city: "Austin",
-        state: "TX",
-        zip: "78701",
+        lineOne: "1234 Innovation Drive",
+        lineTwo: "Suite 500",
+        city: "Denver",
+        state: "CO",
+        zip: "80202",
         country: "US",
         mapLink: "https://goo.gl/maps/example"
     },
     socials: {
-        facebook: "https://www.facebook.com/pixelperfect",
-        instagram: "https://www.instagram.com/pixelperfect"
+        facebook: "https://www.facebook.com/webbercreative",
+        instagram: "https://www.instagram.com/webbercreative",
+        linkedin: "https://www.linkedin.com/company/webbercreative"
     },
-    domain: "https://www.pixelperfect.dev"
+    domain: "https://www.webbercreative.com"
 };
